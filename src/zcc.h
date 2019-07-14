b65890abe8f569cf6a67cdccf5ecc67150660dbc
Yusaku Maezono <yusakumaezono@gmail.com>
2019-07-14 20:52:01 +0900

    Rename codegen.c -> gen_x86.c, ir.c -> gen_ir.c.

--- src/zcc.h
+++ src/zcc.h
@@ -112,7 +112,7 @@ typedef struct Node
 
 Vector *parse(Vector *tokens);
 
-/// ir.c
+/// gen_ir.c
 
 enum
 {
@@ -182,5 +182,5 @@ void dump_ir(Vector *irv);
 extern char *regs[];
 void alloc_regs(Vector *irv);
 
-/// codegen.c
+/// gen_x86.c
 void gen_x86(Vector *fns);
