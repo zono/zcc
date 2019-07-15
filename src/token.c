c2ebb0e7891b287689d949f6bf78cf067053dd6b
Yusaku Maezono <yusakumaezono@gmail.com>
2019-07-15 20:18:41 +0900

    Add "for".

--- src/token.c
+++ src/token.c
@@ -10,13 +10,15 @@ static Token *add_token(Vector *v, int ty, char *input)
   return t;
 }
 
-static Map *keywords;
-
 static struct
 {
   char *name;
   int ty;
 } symbols[] = {
+    {"else", TK_ELSE},
+    {"for", TK_FOR},
+    {"if", TK_IF},
+    {"return", TK_RETURN},
     {"&&", TK_LOGAND},
     {"||", TK_LOGOR},
     {NULL, 0},
@@ -68,13 +70,8 @@ loop:
       while (isalpha(p[len]) || isdigit(p[len]) || p[len] == '_')
         len++;
 
-      char *name = strndup(p, len);
-      int ty = (intptr_t)map_get(keywords, name);
-      if (!ty)
-        ty = TK_IDENT;
-
-      Token *t = add_token(v, ty, p);
-      t->name = name;
+      Token *t = add_token(v, TK_IDENT, p);
+      t->name = strndup(p, len);
       i++;
       p += len;
       continue;
@@ -98,9 +95,5 @@ loop:
 
 Vector *tokenize(char *p)
 {
-  keywords = new_map();
-  map_put(keywords, "if", (void *)TK_IF);
-  map_put(keywords, "else", (void *)TK_ELSE);
-  map_put(keywords, "return", (void *)TK_RETURN);
   return scan(p);
 }
