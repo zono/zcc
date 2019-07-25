c39db4bbedc4aa19fb62a9f8acbff3297e38ff89
Yusaku Maezono <yusakumaezono@gmail.com>
2019-07-25 13:55:52 +0900

    Simplify.

--- src/token.c
+++ src/token.c
@@ -12,19 +12,22 @@ typedef struct Context
 static Context *ctx;
 static Map *keywords;
 
-static char *read_file(char *path)
+static FILE *open_file(char *path)
 {
-  FILE *fp = stdin;
-  if (strcmp(path, "-"))
-  {
-    fp = fopen(path, "r");
-    if (!fp)
-    {
-      perror(path);
-      exit(1);
-    }
-  }
+  if (!strcmp(path, "-"))
+    return stdin;
 
+  FILE *fp = fopen(path, "r");
+  if (!fp)
+  {
+    perror(path);
+    exit(1);
+  }
+  return fp;
+}
+
+static char *read_file(FILE *fp)
+{
   StringBuilder *sb = new_sb();
   char buf[4096];
   for (;;)
@@ -35,8 +38,9 @@ static char *read_file(char *path)
     sb_append_n(sb, buf, nread);
   }
 
-  if (sb->data[sb->len] != '\n')
-    sb_add(sb, '\n');
+  // We want to make sure that a souce file ends with a newline.
+  // Add not only one but two to protect against a backslash at EOF.
+  sb_append(sb, "\n\n");
   return sb_get(sb);
 }
 
@@ -514,7 +518,8 @@ Vector *tokenize(char *path, bool add_eof)
   if (!keywords)
     keywords = keyword_map();
 
-  char *buf = read_file(path);
+  FILE *fp = open_file(path);
+  char *buf = read_file(fp);
   canonicalize_newline(buf);
   remove_backslash_newline(buf);
 
